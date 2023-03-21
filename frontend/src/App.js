4b49fdcc33fa07b80ab6161c1532209949a36db0
Oliveirs <oliveirs@uwindsor.ca>
2023-03-21 11:03:03 -0400

    Adjust movie call to process conditionally

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,76 @@
 // Importing modules
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Button from '@mui/material/Button';
-import AppBar from '@mui/material/AppBar';
-import Movies from "./MovieBox"
+import Button from "@mui/material/Button";
+import AppBar from "@mui/material/AppBar";
+import Movies from "./MovieBox";
 
 function App() {
-    const [data, setData] = useState([{}]);
-    const [toggle, setToggle] = useState(false);
-    
-    const handleClick = () => {
-        setToggle(!toggle);
-    };
-    
-    
-    // Using useEffect for single rendering
-    useEffect(() => {
-        fetch('/recommend').then(
-            res => res.json().then(data => {
-                setData(data)
-            })
-        )
-    }, []);
+  const [data, setData] = useState([{}]);
+  const [toggle, setToggle] = useState(false);
 
-    var dict_rec = data.recommendations;
-    const movie_array = [];
-    var i = 0;
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
 
-    for (var key in dict_rec){
-        movie_array[i] = dict_rec[key][0]
-        i++;
-    }
+  const getRecommendations = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    setData(data);
+  };
 
-    var first_mov_ex = movie_array[0]?.substring(0, movie_array[0].length - 7)
-    first_mov_ex = first_mov_ex?.replace(/\s/g, '+');
+  // Using useEffect for single rendering
+  useEffect(() => {
+    getRecommendations("/recommend");
+  }, []);
 
-    return (
-        <div>
-            <AppBar position="static" title = "">
-                <h3>Recommendation System</h3>
-            </AppBar>
-            <div className="App">
-                <div>{first_mov_ex}</div>
-                <Movies movie_array={first_mov_ex}/>
-                
-                <Button variant="contained" className="Button-stats" onClick={handleClick}>View Stats</Button>
+  var dict_rec = data.recommendations;
+  const movie_array = [];
+  var i = 0;
 
-            <div style={{ display: toggle ? 'block' : 'none' }}>
-                    <p><b>ndcg: </b>{data.ndcg}</p>
-                    <p><b>precision: </b>{data.precision}</p>
-                    <p><b>user_id: </b>{data.user_id}</p>
-            </div>
-            </div>
+  for (var key in dict_rec) {
+    movie_array[i] = dict_rec[key][0];
+    i++;
+  }
+
+  return (
+    <div>
+      <AppBar position="static" title="">
+        <h3>Recommendation System</h3>
+      </AppBar>
+      <div className="App">
+        {movie_array && (
+          <div>
+            <Movies movie_array={movie_array} />
+          </div>
+        )}
+
+        <Button
+          variant="contained"
+          className="Button-stats"
+          onClick={handleClick}
+        >
+          View Stats
+        </Button>
+
+        <div style={{ display: toggle ? "block" : "none" }}>
+          <p>
+            <b>ndcg: </b>
+            {data.ndcg}
+          </p>
+          <p>
+            <b>precision: </b>
+            {data.precision}
+          </p>
+          <p>
+            <b>user_id: </b>
+            {data.user_id}
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-  
+
 export default App;
--- frontend/src/MovieBox.js
+++ frontend/src/MovieBox.js
@@ -1,25 +1,29 @@
-import { useState, useEffect } from 'react';
-import './App.js'
-import MovieCard from './MovieCard.js';
+import { useState, useEffect } from "react";
+import "./App.js";
+import MovieCard from "./MovieCard.js";
 
-const Home = ({movie_array}) => {
-  const [movies, setMovies] = useState();
-    
+const Home = ({ movie_array }) => {
+  const [movies, setMovies] = useState([]);
+  // var first_mov_ex = movie_array[0]?.substring(0, movie_array[0].length - 7);
+  // first_mov_ex = first_mov_ex?.replace(/\s/g, "+");
   const getMovies = async (url) => {
-      const res = await fetch(url);
-      const data = await res.json();
-      setMovies(data.results);
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovies(data.results);
   };
-  
-  useEffect(() => {
-      const MovieUrl = "https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + movie_array;
 
-      getMovies(MovieUrl);
+  useEffect(() => {
+    const MovieUrl =
+      "https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" +
+      movie_array;
+    getMovies(MovieUrl);
   }, [movie_array]);
 
   return (
     <div className="container">
-      <MovieCard movie={movies}/>
+      {movies?.map((movie) => (
+        <MovieCard movie={movie} />
+      ))}
     </div>
   );
 };
